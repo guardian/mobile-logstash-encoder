f76cb19ee05386bb45e4b039724e886d6c612134
Mark Richards <mark.richards@guardian.co.uk>
2018-11-29 16:59:01 +0000

    MSS-554: Adding instance id

--- src/main/scala/com/gu/mobile/logback/MobileLogstashEncoder.scala
+++ src/main/scala/com/gu/mobile/logback/MobileLogstashEncoder.scala
@@ -1,5 +1,6 @@
 package com.gu.mobile.logback
 
+import com.amazonaws.util.EC2MetadataUtils
 import com.gu.{AppIdentity, AwsIdentity}
 import net.logstash.logback.encoder.LogstashEncoder
 import play.api.libs.json.{JsObject, JsString, Json}
@@ -12,20 +13,24 @@ final class MobileLogstashEncoder extends LogstashEncoder {
   def setDefaultAppName(defaultAppName: String): Unit = maybeDefaultAppName = Some(defaultAppName)
 
   private def loadAppStackStageJsObject: JsObject = {
+    val maybeInstanceId: Seq[(String, String)] = Option(EC2MetadataUtils.getInstanceId).map(instanceId =>
+      Seq("ec2_instance" -> instanceId)).getOrElse(Seq()
+    )
     val defaultAppName: String = maybeDefaultAppName.getOrElse(throw new IllegalArgumentException(
       s"""Logback xml must include a defaultAppName like
          |<encoder class="${classOf[MobileLogstashEncoder].getCanonicalName}">
          |  <defaultAppName>APP_NAME_HERE</defaultAppName>
          |</encoder>
       """.stripMargin))
-    JsObject((AppIdentity.whoAmI(defaultAppName = defaultAppName) match {
+    val customFields: Map[String, String] = (AppIdentity.whoAmI(defaultAppName = defaultAppName) match {
       case awsIdentity: AwsIdentity => Map(
         "app" -> awsIdentity.app,
         "stack" -> awsIdentity.stack,
         "stage" -> awsIdentity.stage
       )
       case _ => Map("app" -> defaultAppName)
-    }).mapValues(JsString))
+    }) ++ maybeInstanceId
+    JsObject(customFields.mapValues(JsString))
   }
 
   override def start(): Unit = {
