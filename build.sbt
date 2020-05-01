236065a3551e268c181d941505a2a98e336234c2
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-05-01 11:46:25 +0100

    Spring cleaning:  - Upgrade Scala version  - Upgrade SBT version  - Upgrade Release pipeline version  - Cross publish to scala 2.13  - Drop dependency on Play, use Jackson directly  - Add a unit test to check the serialisation logic

--- build.sbt
+++ build.sbt
@@ -1,6 +1,11 @@
 import sbtrelease.ReleaseStateTransformations._
 
+val scala_2_12: String = "2.12.11"
+val scala_2_13: String = "2.13.2"
+
 lazy val publishSettings = Seq(
+  crossScalaVersions := Seq(scala_2_12, scala_2_13),
+  releaseCrossBuild := true,
   releasePublishArtifactsAction := PgpKeys.publishSigned.value,
   bintrayOrganization := Some("guardian"),
   bintrayRepository := "platforms",
@@ -25,12 +30,14 @@ lazy val root = (project in file("."))
   .settings(
     licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.html")),
     organization := "com.gu",
-    scalaVersion := "2.12.7",
+    scalaVersion := scala_2_12,
     name := "mobile-logstash-encoder",
     libraryDependencies ++= Seq(
-      "com.gu" %% "simple-configuration-core" % "1.5.0",
+      "com.gu" %% "simple-configuration-core" % "1.5.2",
       "net.logstash.logback" % "logstash-logback-encoder" % "5.2",
       "ch.qos.logback" % "logback-core" % "1.2.3",
-      "com.typesafe.play" %% "play-json" % "2.6.10"
+      "com.fasterxml.jackson.core" % "jackson-core" % "2.9.10",
+      "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.10.4",
+      "org.specs2" %% "specs2-core" % "4.8.3" % "test"
     )
   )
--- src/main/scala/com/gu/mobile/logback/MobileLogstashEncoder.scala
+++ src/main/scala/com/gu/mobile/logback/MobileLogstashEncoder.scala
@@ -1,43 +1,82 @@
 package com.gu.mobile.logback
 
+import java.io.StringWriter
+
 import com.amazonaws.util.EC2MetadataUtils
+import com.fasterxml.jackson.core.{JsonFactory, TreeNode}
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
 import com.gu.{AppIdentity, AwsIdentity}
 import net.logstash.logback.encoder.LogstashEncoder
-import play.api.libs.json.{JsObject, JsString, Json}
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
-final class MobileLogstashEncoder extends LogstashEncoder {
+trait JsonConcatenationLogic {
+
+  private val mapper = new ObjectMapper()
+  private val jsonFactory = new JsonFactory(mapper)
+
+  def concatenateEnvToCustomFields(customFields: String, extraFields: Map[String, String]): String = {
+    def parsedTreeNode: Try[TreeNode] = {
+      if (customFields != null && customFields.trim != "") {
+        Try(jsonFactory.createParser(customFields).readValueAsTree[TreeNode]())
+      } else {
+        Success(mapper.createObjectNode())
+      }
+    }
+
+    def appendToObjectNode(treeNode: TreeNode): Try[ObjectNode] = treeNode match {
+      case objectNode: ObjectNode => {
+        extraFields.foreach { case (key, value) => objectNode.put(key, value) }
+        Success(objectNode)
+      }
+      case _ => Failure(new RuntimeException(s"Parsed Json was not an object: ${customFields}"))
+    }
+
+    def serialiseToString(objectNode: ObjectNode): Try[String] = Try {
+      val sw = new StringWriter()
+      val generator = jsonFactory.createGenerator(sw)
+      generator.writeTree(objectNode)
+      sw.toString
+    }
+
+    val concatenatedFields = for {
+      treeNode <- parsedTreeNode
+      objectNode <- appendToObjectNode(treeNode)
+      output <- serialiseToString(objectNode)
+    } yield output
+
+    concatenatedFields match {
+      case Success(result) => result
+      case Failure(exception) => throw exception
+    }
+  }
+}
+
+final class MobileLogstashEncoder extends LogstashEncoder with JsonConcatenationLogic {
   private var maybeDefaultAppName: Option[String] = None
 
   def setDefaultAppName(defaultAppName: String): Unit = maybeDefaultAppName = Some(defaultAppName)
 
-  private lazy val loadAppStackStageJsObject: JsObject = {
+  private lazy val loadAppStackStage: Map[String, String] = {
     val defaultAppName: String = maybeDefaultAppName.getOrElse(throw new IllegalArgumentException(
       s"""Logback xml must include a defaultAppName like
          |<encoder class="${classOf[MobileLogstashEncoder].getCanonicalName}">
          |  <defaultAppName>APP_NAME_HERE</defaultAppName>
          |</encoder>
       """.stripMargin))
-    JsObject(((AppIdentity.whoAmI(defaultAppName = defaultAppName) match {
+    (AppIdentity.whoAmI(defaultAppName = defaultAppName) match {
       case awsIdentity: AwsIdentity => Map(
         "app" -> awsIdentity.app,
         "stack" -> awsIdentity.stack,
         "stage" -> awsIdentity.stage
       )
       case _ => Map("app" -> defaultAppName)
-    }) ++ Option(EC2MetadataUtils.getInstanceId).map("ec2_instance" -> _)).mapValues(JsString))
+    }) ++ Option(EC2MetadataUtils.getInstanceId).map("ec2_instance" -> _)
   }
 
   override def start(): Unit = {
-    this.setCustomFields(Json.stringify(getCustomFieldsAsPlayJsObject.deepMerge(loadAppStackStageJsObject)))
+    this.setCustomFields(concatenateEnvToCustomFields(this.getCustomFields, loadAppStackStage))
     super.start()
   }
-
-  private def getCustomFieldsAsPlayJsObject = Try(Json.parse(getCustomFields))
-    .map(_.validate[JsObject].asOpt)
-    .toOption
-    .flatten
-    .getOrElse(JsObject(Seq()))
-
 }
